e78aaa417df0178919ab873308d7fe03ceb9eaf2
jamie <jamie.levine.phone@gmail.com>
2026-03-08 13:40:47 -0400

    fix: allow Cloudflare beacon in Content-Security-Policy (#19)
    Added https://static.cloudflareinsights.com to the script-src CSP
    directive so the Cloudflare Web Analytics beacon script is no longer
    blocked in production.

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use((_req, res, next) => {
       'Content-Security-Policy',
       [
         "default-src 'self'",
-        "script-src 'self' 'unsafe-inline'",
+        "script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com",
         "connect-src 'self' https://www.googleapis.com https://sheets.googleapis.com https://oauth2.googleapis.com",
         "img-src 'self' https://*.googleusercontent.com data:",
         "style-src 'self' 'unsafe-inline'",
